8e434fb690af50e3451ce7d0e19d9666fdb336ab
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-26 05:05:21 +0700

    update

---
+++ app-redis/app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import {RgetImageAll, RgetImageByAlbum, RgetImageById} from './rediSQL.js'
+
+const port = 3000
+const app = express()
+
+app.use(express.static('public'));
+
+app.get('/r/imgall', async (req, res) => {
+   const result = await RgetImageAll();
+   res.json(result);
+});
+
+app.get('/r/imgalbum/:album', async (req, res) => {
+   const album = req.params.album
+   const result = await RgetImageByAlbum(album);
+   res.json(result);
+});
+
+app.get('/r/imgid/:id', async (req, res) => {
+   const id = req.params.id
+   const result = await RgetImageById(id);
+   res.json(result);
+});
+
+app.listen(port, () => {
+   console.log('App is running on port', port)
+});
