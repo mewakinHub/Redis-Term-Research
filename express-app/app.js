40a50b3bb0c9145f9e2d81d1ca9b691289ffd810
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-26 03:24:30 +0700

    update

--- express-app/app.js
+++ express-app/app.js
@@ -6,18 +6,18 @@ const app = express()
 
 app.use(express.static('public'));
 
-app.get('/imageall', async (req, res) => {
+app.get('/imgall', async (req, res) => {
    const result = await getImageAll();
    res.json(result);
 });
 
-app.get('/imagebyalbum/:album', async (req, res) => {
+app.get('/imgalbum/:album', async (req, res) => {
    const album = req.params.album
    const result = await getImageByAlbum(album);
    res.json(result);
 });
 
-app.get('/imagebyid/:id', async (req, res) => {
+app.get('/imgid/:id', async (req, res) => {
    const id = req.params.id
    const result = await getImageById(id);
    res.json(result);
