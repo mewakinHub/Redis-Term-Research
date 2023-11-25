a4e168afb576054d22c2482415127ecd2c926c47
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-25 17:34:05 +0700

    vite app

--- main/app.js
+++ main/app.js
@@ -2,18 +2,19 @@ import express from 'express'
 import {getAlbum, getImage} from './database.js'
 
 const app = express()
-const port = 8080;
+const port = 8080
 
-app.use(express.json())
+app.use(express.json)
+
+app.get('/app', async (req, res) => {
+   const result = await getAlbum()
+   res.send(result)
+})
 
 app.get('/app/:id', async (req, res) => {
    const id = req.params.id
    const result = await getImage(id)
    res.send(result)
-})
-
-app.post('/notes', async (req, res) => {
-
 })
 
 app.use((err, req, res, next) => {
