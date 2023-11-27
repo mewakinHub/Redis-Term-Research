e3bf4695a5e5d552274f66cb3fc0d5e88f8e30f8
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-27 12:24:44 +0700

    update

--- app-redis/app.js
+++ app-redis/app.js
@@ -36,6 +36,22 @@ app.get('/all', async (req, res) => {
    }
 });
 
+app.get('/album/:album', async (req, res) => {
+   const rdata = await redisCli.get('imgAlbum')
+
+   const album = req.params.album;
+   const [dbdata] = await conn.query('SELECT image FROM images WHERE album=?', [album]);
+   const dbjson = JSON.stringify(dbdata);
+   res.send(dbjson);
+})
+
+app.get('/id/:id', async (req, res) => {
+   const id = req.params.id;
+   const [dbdata] = await conn.query('SELECT image FROM images WHERE id=?', [id]);
+   const dbjson = JSON.stringify(dbdata);
+   res.send(dbjson);
+})
+
 app.listen(port, () => {
    console.log('Server is running on port', port);
 });
--- app-vanilla/app.js
+++ app-vanilla/app.js
@@ -21,6 +21,20 @@ app.get('/all', async (req, res) => {
    res.send(dbjson);
 });
 
+app.get('/album/:album', async (req, res) => {
+   const album = req.params.album;
+   const [dbdata] = await conn.query('SELECT image FROM images WHERE album=?', [album]);
+   const dbjson = JSON.stringify(dbdata);
+   res.send(dbjson);
+})
+
+app.get('/id/:id', async (req, res) => {
+   const id = req.params.id;
+   const [dbdata] = await conn.query('SELECT image FROM images WHERE id=?', [id]);
+   const dbjson = JSON.stringify(dbdata);
+   res.send(dbjson);
+})
+
 app.listen(port, () => {
    console.log('Server is running on port', port);
 });
