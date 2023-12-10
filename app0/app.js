949a9d3754e56c201295b49f98f3250883bb2462
twtrp <134314926+twtrp@users.noreply.github.com>
2023-12-10 22:01:29 +0700

    update

--- app0/app.js
+++ app0/app.js
@@ -10,36 +10,52 @@ const conn = mysql.createConnection({
 
 const app = express();
 
+var loadTime = 0;
+
 //Adjustable variables
 const port = 3000;
 
 app.use(express.static('public'));
 
 app.get('/loadtime/:loadtime', async (req, res) => {
-   const loadtime = req.params.loadtime;
-   console.log('Load time:', loadtime, 'ms');
+   loadTime = req.params.loadtime;
+   console.log('Total load time:', loadTime, 'ms');
+   console.log('---------------');
 });
 
 app.get('/all', async (req, res) => {
+   var startTime = new Date().getTime();
    const [dbdata] = await conn.query('SELECT image FROM images;');
    const dbjson = JSON.stringify(dbdata);
    res.send(dbjson);
+   var endTime = new Date().getTime();
+   var responseTime = endTime - startTime;
+   console.log('Fetch response time:', responseTime, 'ms');
 });
 
 app.get('/album/:album', async (req, res) => {
+   var startTime = new Date().getTime();
    const album = req.params.album;
    const [dbdata] = await conn.query('SELECT image FROM images WHERE album=?', [album]);
    const dbjson = JSON.stringify(dbdata);
    res.send(dbjson);
-})
+   var endTime = new Date().getTime();
+   var responseTime = endTime - startTime;
+   console.log('Fetch response time:', responseTime, 'ms');
+});
 
 app.get('/id/:id', async (req, res) => {
+   var startTime = new Date().getTime();
    const id = req.params.id;
    const [dbdata] = await conn.query('SELECT image FROM images WHERE id=?', [id]);
    const dbjson = JSON.stringify(dbdata);
    res.send(dbjson);
-})
+   var endTime = new Date().getTime();
+   var responseTime = endTime - startTime;
+   console.log('Fetch response time:', responseTime, 'ms');
+});
 
 app.listen(port, () => {
    console.log('Server is running on port', port);
+   console.log('---------------');
 });
--- app1/app.js
+++ app1/app.js
@@ -14,6 +14,8 @@ const redisCli = redis.createClient();
 redisCli.on('error', err => console.log('Redis Client Error', err));
 await redisCli.connect();
 
+var loadTime = 0;
+
 //Adjustable variables
 const port = 3001;
 const TTL = 3600;
@@ -21,61 +23,95 @@ const TTL = 3600;
 app.use(express.static('public'));
 
 app.get('/loadtime/:loadtime', async (req, res) => {
-   const loadtime = req.params.loadtime;
-   console.log('Load time:', loadtime, 'ms');
+   loadTime = req.params.loadtime;
+   console.log('Total load time:', loadTime, 'ms');
+   console.log('---------------');
 });
 
 app.get('/all', async (req, res) => {
-   const rdata = await redisCli.get('img');
-   
+   var startTime = new Date().getTime();
+   const key = 'img';
+   const rdata = await redisCli.get(key);
    if (rdata != null) {
-      console.log('Cache Hit: all');
+      console.log('Key:', key);
+      console.log('Cache: Hit');
       res.send(rdata);
-      redisCli.expire('img', TTL);
+      var endTime = new Date().getTime();
+      var responseTime = endTime - startTime;
+      console.log('Fetch response time:', responseTime, 'ms');
+      redisCli.expire(key, TTL);
    }
    else {
-      console.log('Cache Miss: all');
+      console.log('Key:', key);
+      console.log('Cache: Miss');
       const [dbdata] = await conn.query('SELECT image FROM images;');
       const dbJson = JSON.stringify(dbdata);
-      res.send(dbJson)
-      redisCli.setEx('img', TTL, dbJson);
+      res.send(dbJson);
+      var endTime = new Date().getTime();
+      var responseTime = endTime - startTime;
+      console.log('Fetch response time:', responseTime, 'ms');
+      redisCli.setEx(key, TTL, dbJson);
+      console.log('• setEx done');
    }
 });
 
 app.get('/album/:album', async (req, res) => {
+   var startTime = new Date().getTime();
    const album = req.params.album;
-   const rdata = await redisCli.get(`img?album=${album}`);
+   const key = 'img?album='+album;
+   const rdata = await redisCli.get(key);
    if (rdata != null) {
-      console.log('Cache Hit: album', album);
+      console.log('Key:', key);
+      console.log('Cache: Hit');
       res.send(rdata);
-      redisCli.expire(`img?album=${album}`, TTL);
+      var endTime = new Date().getTime();
+      var responseTime = endTime - startTime;
+      console.log('Fetch response time:', responseTime, 'ms');
+      redisCli.expire(key, TTL);
    }
    else {
-      console.log('Cache Miss: album', album);
+      console.log('Key:', key);
+      console.log('Cache: Miss');
       const [dbdata] = await conn.query('SELECT image FROM images WHERE album=?', [album]);
       const dbJson = JSON.stringify(dbdata);
       res.send(dbJson);
-      redisCli.setEx(`img?album=${album}`, TTL, dbJson)
+      var endTime = new Date().getTime();
+      var responseTime = endTime - startTime;
+      console.log('Fetch response time:', responseTime, 'ms');
+      redisCli.setEx(key, TTL, dbJson);
+      console.log('• setEx done');
    }
 });
 
 app.get('/id/:id', async (req, res) => {
+   var startTime = new Date().getTime();
    const id = req.params.id;
-   const rdata = await redisCli.get(`imgId?id=${id}`);
+   const key = 'img?id='+id;
+   const rdata = await redisCli.get(key);
    if (rdata != null) {
-      console.log('Cache Hit: album', album);
+      console.log('Key:', key);
+      console.log('Cache: Hit');
       res.send(rdata);
-      redisCli.expire(`img?id=${id}`, TTL);
+      var endTime = new Date().getTime();
+      var responseTime = endTime - startTime;
+      console.log('Fetch response time:', responseTime, 'ms');
+      redisCli.expire(key, TTL);
    }
    else {
-      console.log('Cache Miss');
+      console.log('Key:', key);
+      console.log('Cache: Miss');
       const [dbdata] = await conn.query('SELECT image FROM images WHERE id=?', [id]);
       const dbJson = JSON.stringify(dbdata);
       res.send(dbJson);
-      redisCli.setEx(`img?id=${id}`, TTL, dbJson);
+      var endTime = new Date().getTime();
+      var responseTime = endTime - startTime;
+      console.log('Fetch response time:', responseTime, 'ms');
+      redisCli.setEx(key, TTL, dbJson);
+      console.log('• setEx done');
    }
-})
+});
 
 app.listen(port, () => {
    console.log('Server is running on port', port);
+   console.log('---------------');
 });
